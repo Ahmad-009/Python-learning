3e3f0abb72644b79f496254c64792fb9fca27a0a
Ahmad-009 <manj2825@gmail.com>
2025-04-03 19:07:49 +0500

    initial commit

---
+++ Day1.py
@@ -0,0 +1,17 @@
+# """name1 = "Muhammad"
+# name2 = "Ahmad"
+
+# print(name1 + " " + name2) # concatenation"""
+
+# name = input("Whats ur name? ")
+# # using concatenation we do complex printing like this
+# print("Hello" + " "  + name) 
+# length = len(name)
+# print("Your name is " + name + " " +  "and the length of the name is " + str(length))
+
+#Day1 project below:
+
+print("Welcome to the band name generator \n")
+city_name = input("Whats the name of the city u grew up in? \n")
+pet_name = input("Whats your pet name? \n")
+print("Your band name could be " + city_name + " " + pet_name)
